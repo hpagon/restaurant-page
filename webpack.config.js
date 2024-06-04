13502e4f0faff4d5e249c980b17c9823077c271d
Heber <116190313+hpagon@users.noreply.github.com>
2024-06-04 13:25:42 -0700

    Setup npm installations/webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    watchFiles: ["src/index.html"],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
