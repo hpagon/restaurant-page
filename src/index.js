23fe65e59d52fbf0482e879b40e139c0629792fb
Heber <116190313+hpagon@users.noreply.github.com>
2024-06-04 22:39:08 -0700

    Disabled tab buttons for active tabs

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import "./style.css";
 class Controller {
   #buttons = document.querySelectorAll("header nav button");
   #content = document.querySelector("#content");
+  #disabledButton = this.#buttons[0];
 
   constructor() {
     for (const button of this.#buttons) {
       button.addEventListener("click", (e) => {
         this.clear();
+        this.unfreeze();
         this.freeze(e.target);
         switch (e.target.innerHTML) {
           case "Home":
@@ -26,6 +28,7 @@ class Controller {
       });
     }
     initial();
+    this.freeze(this.#disabledButton);
   }
   //clears the content div
   clear() {
@@ -33,7 +36,12 @@ class Controller {
   }
   //disables click on button
   freeze(button) {
-    button.setAttribute("disabled", "disabled");
+    button.setAttribute("disabled", true);
+    this.#disabledButton = button;
+  }
+  //allow clicking funtionality on button again
+  unfreeze() {
+    this.#disabledButton.removeAttribute("disabled");
   }
 }
 
