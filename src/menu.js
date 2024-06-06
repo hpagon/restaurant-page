4693913fd4f674a8b991133ca49ce5d45fb9fbd8
Heber <116190313+hpagon@users.noreply.github.com>
2024-06-06 11:20:32 -0700

    Added font

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,13 @@
 //exports a function used to create elements in home page
 export default function () {
     const content = document.querySelector("#content");
-    
-    let homepage = document.createElement("div");
-    homepage.innerHTML = "This is the Menu page.";
-
-    content.appendChild(homepage);
+    let menu = document.createElement("div");
+    //create dom elements
+    const header = document.createElement("h2");
+    //add classes/id to elements
+    //add content to elements
+    header.innerHTML = "Menu"
+    //add elements to page
+    menu.append(header);
+    content.appendChild(menu);
 }
